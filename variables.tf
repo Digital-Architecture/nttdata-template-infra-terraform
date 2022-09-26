a8c2c0bfe830f810dd26eaf6a717a79687a329ae
mcianci2020 <marcos.cianci@gmail.com>
2022-09-26 16:42:34 -0300

    fix...

--- variables.tf
+++ variables.tf
@@ -1,2 +1,8 @@
 # NTTDATA - DIGITAL ARCHITECTURE
 # Create: Marcos Cianci - mlopesci@emeal.nttdata.com
+
+variable region {
+  type        = string
+  default     = ""
+  description = "description"
+}
